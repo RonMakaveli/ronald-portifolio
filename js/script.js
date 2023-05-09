b65d042ae742812b85c4e9bb83c170956c9e5b22
Ronald Gustavo Nascimento Rodrigues Pereira <ronaald_gustavoo@hotmail.com>
2023-05-08 22:44:07 -0300

    Função para imprimir os devidos valores na tela

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ tabsContainer.addEventListener("click" , (e)=>{
 document.addEventListener("click", (e) =>{
     if(e.target.classList.contains("view-project-btn")){
         togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
     };
 });
 
@@ -26,3 +27,14 @@ function togglePortfolioPopup(){
 };
 
 document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src = 
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
